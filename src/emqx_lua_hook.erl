10938c12e08c62160e70e25bfbf03c682a91888a
周子博 <349832309@qq.com>
2019-03-22 16:14:59 +0800

    Export format

--- src/emqx_lua_hook.erl
+++ src/emqx_lua_hook.erl
@@ -19,23 +19,23 @@
 -include("emqx_lua_hook.hrl").
 -include_lib("luerl/src/luerl.hrl").
 
--export([  start_link/0
-         , stop/0
-         ]).
+-export([ start_link/0
+        , stop/0
+        ]).
 
--export([  load_scripts/0
-         , unload_scrips/0
-         , load_script/1
-         , unload_script/1
-         ]).
+-export([ load_scripts/0
+        , unload_scrips/0
+        , load_script/1
+        , unload_script/1
+        ]).
 
--export([  init/1
-         , handle_call/3
-         , handle_cast/2
-         , handle_info/2
-         , terminate/2
-         , code_change/3
-         ]).
+-export([ init/1
+        , handle_call/3
+        , handle_cast/2
+        , handle_info/2
+        , terminate/2
+        , code_change/3
+        ]).
 
 -export([lua_dir/0]).
 
